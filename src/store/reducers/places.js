29d1dfa3b5a8743dcfe8d30b5e8377b25d79607b
MaeganW <maeganwomble@gmail.com>
2018-04-10 10:08:42 -0500

    adding redux

---
+++ src/store/reducers/places.js
@@ -0,0 +1,13 @@
+const initialState = {
+  places: [],
+  selectedPlace: null
+}
+
+const reducer = (oldState = initialState, action) => {
+  switch (action.type) {
+    default:
+      return oldState;
+  }
+}
+
+export default reducer;
