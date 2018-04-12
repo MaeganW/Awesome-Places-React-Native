01d4e883066b253a3959121254d8b60c782918b6
MaeganW <maeganwomble@gmail.com>
2018-04-12 15:51:06 -0500

    android requires icons for tabs - how to use vector-icons Icon promises to get icon source

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -1,20 +1,28 @@
 import { Navigation } from 'react-native-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const startTabs = () => {
-  Navigation.startTabBasedApp({
-    tabs: [
-      {
-        screen: 'amazing-places.FindPlaceScreen',
-        label: 'Find Place',
-        title: 'Find Place'
-      },
-      {
-        screen: 'amazing-places.SharePlaceScreen',
-        label: 'Share Place',
-        title: 'Share Place'
-      }
-    ]
-  });
+  Promise.all([
+    Icon.getImageSource('md-map', 30),
+    Icon.getImageSource('ios-share-alt', 30)
+  ]).then(imgSources => {
+    Navigation.startTabBasedApp({
+      tabs: [
+        {
+          screen: 'amazing-places.FindPlaceScreen',
+          label: 'Find Place',
+          title: 'Find Place',
+          icon: imgSources[0]
+        },
+        {
+          screen: 'amazing-places.SharePlaceScreen',
+          label: 'Share Place',
+          title: 'Share Place',
+          icon: imgSources[1]
+        }
+      ]
+    });
+  })
 }
 
 export default startTabs;
